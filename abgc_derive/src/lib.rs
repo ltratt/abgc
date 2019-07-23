2be22bbeb865b181542b769c10274a982ae23f58
Laurence Tratt <laurie@tratt.net>
2019-07-23 17:45:43 +0100

    Add a GcLayout derive macro.

--- abgc_derive/src/lib.rs
+++ abgc_derive/src/lib.rs
@@ -1,7 +1,30 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+// Copyright (c) 2019 King's College London created by the Software Development Team
+// <http://soft-dev.org/>
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0>, or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, or the UPL-1.0 license <http://opensource.org/licenses/UPL>
+// at your option. This file may not be copied, modified, or distributed except according to those
+// terms.
+
+extern crate proc_macro;
+
+use crate::proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro_derive(GcLayout)]
+pub fn gclayout_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident = &input.ident;
+    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+    let expanded = quote! {
+        impl #impl_generics ::abgc::GcLayout for #ident #ty_generics #where_clause {
+            fn layout(&self) -> ::std::alloc::Layout {
+                ::std::alloc::Layout::new::<#ident<#ty_generics>>()
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
 }
